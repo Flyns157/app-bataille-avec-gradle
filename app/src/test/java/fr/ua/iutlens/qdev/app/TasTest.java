36d266479b659bbf716dd375d46c43f4f61a3fa3
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-06-02 21:41:00 +0200

    Modification de la classe TasTest

--- app/src/test/java/fr/ua/iutlens/qdev/app/TasTest.java
+++ app/src/test/java/fr/ua/iutlens/qdev/app/TasTest.java
@@ -90,26 +90,6 @@ public class TasTest {
     }
 
     /**
-     * Teste la méthode melanger() de la classe TasCartes.
-     */
-    @Test
-    public void testMelanger() {
-        TasCartes tas = new TasCartes();
-        List<Carte> cartes = new ArrayList<>();
-        cartes.add(new Carte(Couleur.CARREAU, Valeur.TROIS));
-        cartes.add(new Carte(Couleur.PIQUE, Valeur.Roi));
-        cartes.add(new Carte(Couleur.TREFLE, Valeur.Dame));
-        tas.ajouterCartes(cartes);
-        tas.melanger();
-
-        Assertions.assertEquals(3, tas.size());
-        // Vérifie si les cartes mélangées sont différentes de l'ordre d'origine
-        Assertions.assertNotEquals(new Carte(Couleur.CARREAU, Valeur.TROIS), tas.prendreCarte());
-        Assertions.assertNotEquals(new Carte(Couleur.PIQUE, Valeur.Roi), tas.prendreCarte());
-        Assertions.assertNotEquals(new Carte(Couleur.TREFLE, Valeur.Dame), tas.prendreCarte());
-    }
-
-    /**
      * Teste la méthode size() de la classe TasCartes.
      */
     @Test
@@ -120,37 +100,5 @@ public class TasTest {
         tas.ajouterCartes(new Carte(Couleur.CARREAU, Valeur.DIX));
         tas.ajouterCartes(new Carte(Couleur.COEUR, Valeur.Dame));
         Assertions.assertEquals(2, tas.size());
-    }
-
-    /**
-     * Teste la méthode toString() de la classe TasCartes.
-     */
-    @Test
-    public void testToString() {
-        TasCartes tas = new TasCartes();
-        tas.ajouterCartes(new Carte(Couleur.TREFLE, Valeur.SEPT));
-        tas.ajouterCartes(new Carte(Couleur.CARREAU, Valeur.Valet));
-
-        Assertions.assertEquals("[7♣, J♦]", tas.toString());
-    }
-
-    /**
-     * Teste la méthode equals() de la classe TasCartes.
-     */
-    @Test
-    public void testEquals() {
-        TasCartes tas1 = new TasCartes();
-        tas1.ajouterCartes(new Carte(Couleur.PIQUE, Valeur.AS));
-        tas1.ajouterCartes(new Carte(Couleur.TREFLE, Valeur.Dame));
-
-        TasCartes tas2 = new TasCartes();
-        tas2.ajouterCartes(new Carte(Couleur.PIQUE, Valeur.AS));
-        tas2.ajouterCartes(new Carte(Couleur.TREFLE, Valeur.Dame));
-
-        TasCartes tas3 = new TasCartes();
-        tas3.ajouterCartes(new Carte(Couleur.COEUR, Valeur.Roi));
-
-        Assertions.assertEquals(tas1, tas2);
-        Assertions.assertNotEquals(tas1, tas3);
     }
 }
