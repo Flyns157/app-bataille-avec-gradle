71d6541fa2c5d5445cd1dbb85318ffcd2926c7fb
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-06-02 22:11:39 +0200

    Modification de la classe TasFactoryTest

--- app/src/test/java/fr/ua/iutlens/qdev/app/TasFactoryTest.java
+++ app/src/test/java/fr/ua/iutlens/qdev/app/TasFactoryTest.java
@@ -2,19 +2,18 @@ package fr.ua.iutlens.qdev.app;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 public class TasFactoryTest {
 
-    /**
-     * Teste la méthode jeu52Cartes() de la classe TasFactory.
-     */
+    @Mock
+    private ITas tasMock;
+
     @Test
     public void testJeu52Cartes() {
         ITas tas = TasFactory.jeu52Cartes();
@@ -22,9 +21,6 @@ public class TasFactoryTest {
         Assertions.assertEquals(52, tas.size());
     }
 
-    /**
-     * Teste la méthode jeu52CartesShuffle() de la classe TasFactory.
-     */
     @Test
     public void testJeu52CartesShuffle() {
         ITas tas = TasFactory.jeu52CartesShuffle();
@@ -32,32 +28,25 @@ public class TasFactoryTest {
         Assertions.assertEquals(52, tas.size());
     }
 
-    /**
-     * Teste la méthode jeu52CartesShuffle(long seed) de la classe TasFactory.
-     */
     @Test
     public void testJeu52CartesShuffleWithSeed() {
-        long seed = 12345L;
-        ITas tas = TasFactory.jeu52CartesShuffle(seed);
+        ITas tas = TasFactory.jeu52CartesShuffle(123L);
         Assertions.assertNotNull(tas);
         Assertions.assertEquals(52, tas.size());
     }
 
-    /**
-     * Teste la méthode splitJeu52CartesShuffle(int nbTas, long seed) de la classe TasFactory.
-     */
     @Test
     public void testSplitJeu52CartesShuffle() {
         int nbTas = 4;
-        long seed = 12345L;
+        long seed = 123L;
+
         ITas[] tasArray = TasFactory.splitJeu52CartesShuffle(nbTas, seed);
+
         Assertions.assertNotNull(tasArray);
         Assertions.assertEquals(nbTas, tasArray.length);
-
-        int totalCards = 0;
         for (ITas tas : tasArray) {
-            totalCards += tas.size();
+            Assertions.assertNotNull(tas);
+            Assertions.assertEquals(13, tas.size());
         }
-        Assertions.assertEquals(52, totalCards);
     }
 }
