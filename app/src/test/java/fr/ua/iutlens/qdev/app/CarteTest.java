4f92f97d6e9a5d188224776172dd29cbaf916d0a
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-06-02 20:59:24 +0200

    Ajout d'une classe TasTest et refonte totale de la classe CarteTest et ajout de commentaires dans cette dernière

--- app/src/test/java/fr/ua/iutlens/qdev/app/CarteTest.java
+++ app/src/test/java/fr/ua/iutlens/qdev/app/CarteTest.java
@@ -1,41 +1,53 @@
 package fr.ua.iutlens.qdev.app;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class CarteTest {
+public class CarteTest {
 
     @Test
-    void testConstructor() {
-        Carte carte = new Carte(Couleur.CARREAU, Valeur.AS);
-        assertEquals(Couleur.CARREAU, carte.getCouleur());
-        assertEquals(Valeur.AS, carte.getValeur());
+    public void testConstructeur() {
+        // Teste le constructeur de la classe Carte
+        Carte carte = new Carte(Couleur.PIQUE, Valeur.AS);
+        Assertions.assertEquals(Couleur.PIQUE, carte.getCouleur());
+        Assertions.assertEquals(Valeur.AS, carte.getValeur());
     }
 
     @Test
-    void testFromString() {
-        Carte carte = Carte.fromString("CA");
-        assertEquals(Couleur.CARREAU, carte.getCouleur());
-        assertEquals(Valeur.AS, carte.getValeur());
+    public void testFromString() {
+        // Teste la méthode fromString() de la classe Carte
+        Carte carte = Carte.fromString("S2");
+        Assertions.assertEquals(Couleur.PIQUE, carte.getCouleur());
+        Assertions.assertEquals(Valeur.DEUX, carte.getValeur());
     }
 
     @Test
-    void testToString() {
-        Carte carte = new Carte(Couleur.CARREAU, Valeur.AS);
-        assertEquals("CA", carte.toString());
+    public void testToString() {
+        // Teste la méthode toString() de la classe Carte
+        Carte carte = new Carte(Couleur.COEUR, Valeur.Dame);
+        Assertions.assertEquals("CD", carte.toString());
     }
 
     @Test
-    void testEquals() {
-        Carte carte1 = new Carte(Couleur.CARREAU, Valeur.AS);
-        Carte carte2 = new Carte(Couleur.CARREAU, Valeur.AS);
-        assertTrue(carte1.equals(carte2));
+    public void testEquals() {
+        // Teste la méthode equals() de la classe Carte
+        Carte carte1 = new Carte(Couleur.TREFLE, Valeur.Valet);
+        Carte carte2 = new Carte(Couleur.TREFLE, Valeur.Valet);
+        Carte carte3 = new Carte(Couleur.CARREAU, Valeur.Valet);
+
+        Assertions.assertEquals(carte1, carte2); // Vérifie que les deux cartes sont égales
+        Assertions.assertNotEquals(carte1, carte3); // Vérifie que les deux cartes ne sont pas égales
     }
 
     @Test
-    void testCompareTo() {
-        Carte carte1 = new Carte(Couleur.CARREAU, Valeur.AS);
-        Carte carte2 = new Carte(Couleur.PIQUE, Valeur.Roi);
-        assertTrue(carte1.compareTo(carte2) < 0);
+    public void testCompareTo() {
+        // Teste la méthode compareTo() de la classe Carte
+        Carte carte1 = new Carte(Couleur.TREFLE, Valeur.AS);
+        Carte carte2 = new Carte(Couleur.CARREAU, Valeur.DEUX);
+        Carte carte3 = new Carte(Couleur.COEUR, Valeur.AS);
+
+        Assertions.assertTrue(carte1.compareTo(carte2) > 0); // Vérifie que carte1 est supérieure à carte2
+        Assertions.assertTrue(carte2.compareTo(carte3) < 0); // Vérifie que carte2 est inférieure à carte3
+        Assertions.assertEquals(0, carte1.compareTo(carte3)); // Vérifie que carte1 est égale à carte3
     }
 }
